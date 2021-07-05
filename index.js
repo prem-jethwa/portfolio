c317d8bdb72e3cf71aa07276b8075bcb307103ee
prem-jethwa <premjethwa788@gmail.com>
2021-07-05 16:45:17 +0530

    route test

---
+++ index.js
@@ -0,0 +1,26 @@
+require("./db/mongoose");
+const express = require("express");
+const cors = require("cors");
+// const User = require("./model/user");
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  try {
+    const { message, name, email } = req.body;
+
+    // if (!message || !name || !email) throw new Error("invalid Inputs");
+
+    // const user = new User({ message, name, email });
+
+    res.status(201).send({ message, name, email }, req.body);
+  } catch (err) {
+    res.status(400).send({ type: "error", message: err.message });
+  }
+});
+
+app.listen(port, console.log);
