bfb0eef3dfb8f1090bf51b1465fbdf5ac61b1e99
prem-jethwa <premjethwa788@gmail.com>
2021-08-22 22:34:17 +0530

    added admin control

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,4 +4,6 @@ mongooose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
+  keepAlive: true,
+  keepAliveInitialDelay: 1000000,
 });
